2473a074380d257b1a374b71b55fd1759da3d972
Matteo Bertucci <matteobertucci2004@gmail.com>
2020-12-01 10:32:12 +0100

    Add day 1 part 2 solution

--- day-1/day-1.py
+++ day-1/day-1.py
@@ -10,5 +10,15 @@ def solution_1(data: str) -> int:
                 return elem1 * elem2
 
 
+def solution_2(data: str) -> int:
+    data = parse_data(data)
+
+    for i1, elem1 in enumerate(data):
+        for i2, elem2 in enumerate(data[i1 + 1:]):
+            for elem3 in data[i2 + 1:]:
+                if elem1 + elem2 + elem3 == 2020:
+                    return elem1 * elem2 * elem3
+
+
 def parse_data(data: str) -> Tuple[int]:
     return tuple(int(line) for line in data.split('\n'))
